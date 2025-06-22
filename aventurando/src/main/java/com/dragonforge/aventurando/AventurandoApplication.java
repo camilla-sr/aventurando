028bf7c9f16ac2b62d7e7ecf0d719c2dfa6ad9f0
camilla-sr <camilla.naakh7@gmail.com>
2025-06-22 18:04:51 -0300

    add: esqueleto do projeto

---
+++ aventurando/src/main/java/com/dragonforge/aventurando/AventurandoApplication.java
@@ -0,0 +1,13 @@
+package com.dragonforge.aventurando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AventurandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AventurandoApplication.class, args);
+	}
+
+}
---
+++ aventurando/src/test/java/com/dragonforge/aventurando/AventurandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragonforge.aventurando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AventurandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
