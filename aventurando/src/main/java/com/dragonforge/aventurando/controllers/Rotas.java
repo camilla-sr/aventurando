d19eb8c288047aad1f55d16f9fc958d0ffb6dd37
camilla-sr <camilla.sreis2@gmail.com>
2025-06-27 04:43:20 -0300

    add: index do jogador referenciada na controller de rotas

--- aventurando/src/main/java/com/dragonforge/aventurando/controllers/Rotas.java
+++ aventurando/src/main/java/com/dragonforge/aventurando/controllers/Rotas.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Rotas {
-
 	@GetMapping("login")
 	public String login() {
 		return "login";
+	}
+	
+	@GetMapping("index")
+	public String indexJogador() {
+		return "index_jogador";
 	}
 }
