80a763f75abb15b001986de848a7f7019addae08
camilla-sr <camilla.sreis2@gmail.com>
2025-07-03 01:05:18 -0300

    fix: corrigidos métodos de query específicas para jogador e mestre

--- aventurando/src/main/java/com/dragonforge/aventurando/central/repositorios/UsuarioRepository.java
+++ aventurando/src/main/java/com/dragonforge/aventurando/central/repositorios/UsuarioRepository.java
@@ -2,20 +2,17 @@ package com.dragonforge.aventurando.central.repositorios;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.dragonforge.aventurando.central.models.Usuario;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, Integer>{
 	Usuario findByUsuario(String usuario);
 	
+	Usuario findByAcesso(String acesso);
+	
 	Optional<Usuario> findById(Integer id);
-	
-	@Query("select * from usuario where acesso = 'jogador'")
-	Iterable<Usuario> findAllJogador();
-	
-	@Query("select * from usuario where acesso = 'mestre'")
-	Iterable<Usuario> findAllMestre();
-	
+
 	Iterable<Usuario> findAll();
+	
+	Iterable<Usuario> findByIdAndAcesso(Integer id, String acesso);
 }
