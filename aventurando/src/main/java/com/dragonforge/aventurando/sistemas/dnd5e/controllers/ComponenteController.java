91c797529ea24e4360db59e5efea7e43a86cb1f6
camilla-sr <camilla.sreis2@gmail.com>
2025-07-04 04:04:57 -0300

    add: tabela dinâmica de componentes agora inclui cabeçalho e barra de busca com atalho de teclado

--- aventurando/src/main/java/com/dragonforge/aventurando/sistemas/dnd5e/controllers/ComponenteController.java
+++ aventurando/src/main/java/com/dragonforge/aventurando/sistemas/dnd5e/controllers/ComponenteController.java
@@ -3,6 +3,9 @@ package com.dragonforge.aventurando.sistemas.dnd5e.controllers;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dragonforge.aventurando.central.repositorios.UsuarioRepository;
 import com.dragonforge.aventurando.sistemas.dnd5e.repositorios.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -60,7 +64,16 @@ public class ComponenteController {
 	@Autowired
 	private TracoRepository repoTraco;
 	@Autowired
-	private VinculoRepository repoLink;
+	private VinculoRepository repoLink;	
+	@Autowired
+    private ObjectMapper objectMapper;
+
+	
+    private List<Map<String, Object>> converter(Iterable<?> dados) {
+        return StreamSupport.stream(dados.spliterator(), false)
+                .map(dado -> objectMapper.convertValue(dado, Map.class))
+                .collect(Collectors.toList());
+    }
 	
 	@GetMapping("/{componente}")
 	public String exibirTabela(@PathVariable String componente, Model model, HttpSession session) {
@@ -70,140 +83,140 @@ public class ComponenteController {
 		switch(componente.toLowerCase()) {
 			case "antecedentes":
 				model.addAttribute("titulo", "Antecedentes");
-				model.addAttribute("dados", repoAntec.findAll());
+				model.addAttribute("dados", converter(repoAntec.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Descricão"));
 				campos.addAll(Arrays.asList("id", "descricao"));
 				break;
 			case "armas":
 				model.addAttribute("titulo", "Armas");
-				model.addAttribute("dados", repoWeapon.findAll());
+				model.addAttribute("dados", converter(repoWeapon.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Nome", "Dano", "Preço", "Moeda", "Peso", "Propriedade"));
 				campos.addAll(Arrays.asList("id", "nome", "dano", "preco", "moeda", "peso", "propriedade"));
 				break;
 			case "armaduras":
 				model.addAttribute("titulo", "Armaduras");
-				model.addAttribute("dados", repoArmor.findAll());
+				model.addAttribute("dados", converter(repoArmor.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Tipo", "Preco", "Moeda", "CA", "Modificador", "Requisito", "Desvantagem", "Peso"));
 				campos.addAll(Arrays.asList("id", "tipo", "preco", "moeda", "ca", "modificador", "requisito", "desvantagem", "peso"));
 				break;
 			case "arquetipos":
 				model.addAttribute("titulo", "Arquétipos");
-				model.addAttribute("dados", repoArq.findAll());
+				model.addAttribute("dados", converter(repoArq.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Classe", "Título", "Descrição"));
 				campos.addAll(Arrays.asList("id", "classe", "titulo", "descricao"));
 				break;
 			case "bugigangas":
 				model.addAttribute("titulo", "Bugigangas");
-				model.addAttribute("dados", repoBugi.findAll());
+				model.addAttribute("dados", converter(repoBugi.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Descrição"));
 				campos.addAll(Arrays.asList("id", "descricao"));
 				break;
 			case "classes":
 				model.addAttribute("titulo", "Classes");
-				model.addAttribute("dados", repoClasse.findAll());
+				model.addAttribute("dados", converter(repoClasse.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Classe", "Descrição", "Habilidade Primária", "Dado de vida",
 											"Proficiencia em Arma ou Armadura", "Proficiencia em Teste de Resistência"));
 				campos.addAll(Arrays.asList("id", "classe", "descricao", "habprimaria", "dadovida", "parma", "pteste"));
 				break;
 			case "dados":
 				model.addAttribute("titulo", "Dados");
-				model.addAttribute("dados", repoDice.findAll());
+				model.addAttribute("dados", converter(repoDice.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Tipo"));
 				campos.addAll(Arrays.asList("id", "tipo"));
 				break;
 			case "defeitos":
 				model.addAttribute("titulo", "Defeitos");
-				model.addAttribute("dados", repoDef.findAll());
+				model.addAttribute("dados", converter(repoDef.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Antecedente", "Descrição"));
 				campos.addAll(Arrays.asList("id", "antecedente", "descricao"));
 				break;
 			case "equipamentos":
 				model.addAttribute("titulo", "Equipamentos");
-				model.addAttribute("dados", repoEquip.findAll());
+				model.addAttribute("dados", converter(repoEquip.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Item", "Preço", "Moeda", "Peso"));
 				campos.addAll(Arrays.asList("id", "item", "preco", "moeda", "peso"));
 				break;
 			case "ferramentas":
 				model.addAttribute("titulo", "Ferramentas");
-				model.addAttribute("dados", repoTool.findAll());
+				model.addAttribute("dados", converter(repoTool.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Ferramenta", "Descrição", "Preço", "Moeda", "Peso"));
 				campos.addAll(Arrays.asList("id", "ferramenta", "descricao", "preco", "moeda", "peso"));
 				break;
 			case "glossario":
 				model.addAttribute("titulo", "Glossário de Magias");
-				model.addAttribute("dados", repoGloss.findAll());
+				model.addAttribute("dados", converter(repoGloss.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Magia", "Nível", "Descrição"));
 				campos.addAll(Arrays.asList("id", "magia", "nivel", "descricao"));
 				break;
 			case "habchave":
 				model.addAttribute("titulo", "Habilidades Chave");
-				model.addAttribute("dados", repoGloss.findAll());
+				model.addAttribute("dados", converter(repoGloss.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Habilidade"));
 				campos.addAll(Arrays.asList("id", "habilidade"));
 				break;
 			case "ideais":
 				model.addAttribute("titulo", "Ideais");
-				model.addAttribute("dados", repoIdeal.findAll());
+				model.addAttribute("dados", converter(repoIdeal.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Antecedente", "Descrição"));
 				campos.addAll(Arrays.asList("id", "antecedente", "descricao"));
 				break;
 			case "idiomas":
 				model.addAttribute("titulo", "Idiomas");
-				model.addAttribute("dados", repoIdi.findAll());
+				model.addAttribute("dados", converter(repoIdi.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Descrição", "Alfabeto"));
 				campos.addAll(Arrays.asList("id", "descricao", "alfabeto"));
 				break;
 			case "jogadores":
 				model.addAttribute("titulo", "Jogadores");
-				model.addAttribute("dados", repoUser.findByAcesso("jogador"));
+				model.addAttribute("dados", converter(repoUser.findByAcesso("jogador")));
 				cabecalho.addAll(Arrays.asList("ID", "Nome", "Usuário", "Personagens"));
 				campos.addAll(Arrays.asList("id", "nome", "usuario", "personagens"));
 				break;
 			case "mestres":
 				model.addAttribute("titulo", "Mestres");
-				model.addAttribute("dados", repoUser.findByAcesso("mestre"));
+				model.addAttribute("dados", converter(repoUser.findByAcesso("mestre")));
 				cabecalho.addAll(Arrays.asList("ID", "Nome", "Usuário", "Campanhas"));
 				campos.addAll(Arrays.asList("id", "nome", "usuario", "campanhas"));
 				break;
 			case "moedas":
 				model.addAttribute("titulo", "Moedas");
-				model.addAttribute("dados", repoMoeda.findAll());
+				model.addAttribute("dados", converter(repoMoeda.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Moeda", "Peso"));
 				campos.addAll(Arrays.asList("id", "moeda", "peso"));
 				break;
 			case "racas":
 				model.addAttribute("titulo", "Raças");
-				model.addAttribute("dados", repoRaca.findAll());
+				model.addAttribute("dados", converter(repoRaca.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Raça", "Deslocamento"));
 				campos.addAll(Arrays.asList("id", "raca", "deslocamento"));
 				break;
 			case "talentos":
 				model.addAttribute("titulo", "Talentos");
-				model.addAttribute("dados", repoTalent.findAll());
+				model.addAttribute("dados", converter(repoTalent.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Talento", "Descrição", "Requisito"));
 				campos.addAll(Arrays.asList("id", "talento", "descricao", "requisito"));
 				break;
 			case "tendencias":
 				model.addAttribute("titulo", "Tendências");
-				model.addAttribute("dados", repoTend.findAll());
+				model.addAttribute("dados", converter(repoTend.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Descrição"));
 				campos.addAll(Arrays.asList("id", "descricao"));
 				break;
 			case "danos":
 				model.addAttribute("titulo", "Tipos de Dano");
-				model.addAttribute("dados", repoHit.findAll());
+				model.addAttribute("dados", converter(repoHit.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Descrição"));
 				campos.addAll(Arrays.asList("id", "descricao"));
 				break;
 			case "tracos":
 				model.addAttribute("titulo", "Traços de Personalidadde");
-				model.addAttribute("dados", repoTraco.findAll());
+				model.addAttribute("dados", converter(repoTraco.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Antecedente", "Descrição"));
 				campos.addAll(Arrays.asList("id", "antecedente", "descricao"));
 				break;
 			case "vinculos":
 				model.addAttribute("titulo", "Vínculos");
-				model.addAttribute("dados", repoLink.findAll());
+				model.addAttribute("dados", converter(repoLink.findAll()));
 				cabecalho.addAll(Arrays.asList("ID", "Antecedente", "Descrição"));
 				campos.addAll(Arrays.asList("id", "antecedente", "descricao"));
 				break;
